384a2f45113c0985b27e95eb52a3d2ab8a23ffa4
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-11-14 21:17:48 +0300

    Initial commit

---
+++ Assets/Scripts/InvokeScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvokeScript : MonoBehaviour
+{
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("SpawnObject", 2);
+    }
+
+    void SpawnObject()
+    {
+        Instantiate(target, new Vector3(0, 2, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
